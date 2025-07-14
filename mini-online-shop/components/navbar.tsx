1f35f1c9c7003651a2a62c8c10ecbfa4a028d492
Borhan <iborihan91@gmail.com>
2025-07-14 17:45:31 +0330

    adding Material UI icon

---
+++ mini-online-shop/components/navbar.tsx
@@ -0,0 +1,12 @@
+import { headers } from "next/headers";
+import AccountCircleOutlinedIcon from '@mui/icons-material/AccountCircleOutlined';
+
+export default function Navbar(){
+    return(
+        <header>
+            <div></div>
+            <div></div>
+            <div></div>
+        </header>
+    );
+}
