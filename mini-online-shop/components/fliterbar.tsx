0229c599ae0f0988ec6a5327276fe6178184af93
Borhan <iborihan91@gmail.com>
2025-07-16 13:19:02 +0330

    Update filterbar margin

--- mini-online-shop/components/fliterbar.tsx
+++ mini-online-shop/components/fliterbar.tsx
@@ -3,7 +3,7 @@ import CategoryFilter from "./categoryfilter";
 
 export default function Filterbar() {
   return (
-    <section className="flex flex-row items-center mt-30 mx-15 gap-20">
+    <section className="flex flex-row items-center mt-15 mx-15 gap-20">
       <Search />
       <CategoryFilter />
     </section>
