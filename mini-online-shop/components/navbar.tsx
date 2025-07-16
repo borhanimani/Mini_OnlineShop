c1a6892d015a83482c23198d4222e80c41671672
Borhan <iborihan91@gmail.com>
2025-07-16 16:57:21 +0330

    Create utils folder and project-types file

--- mini-online-shop/components/navbar.tsx
+++ mini-online-shop/components/navbar.tsx
@@ -4,7 +4,7 @@ import AccountCircleOutlinedIcon from '@mui/icons-material/AccountCircleOutlined
 export default function Navbar() {
   return (
     <header className=" flex flex-row justify-between border-3 h-1/8 items-center color-gray">
-      <AccountCircleOutlinedIcon className='' sx={{ width: 45, height: 45, color: "rgba(87, 86, 86, 1)", fontSize: "small"}} />
+      <AccountCircleOutlinedIcon className='border-1' sx={{ width: 45, height: 45, color: "rgba(87, 86, 86, 1)", fontSize: "small"}} />
       <h1 className="font-bold text-red-500">Online Shop</h1>
       <div>
         cart
---
+++ mini-online-shop/utils/project-types.ts
@@ -0,0 +1,14 @@
+export type Product = {
+    id: number;
+    title: string;
+    description: string;
+    price: number;
+}
+
+export type CartItem = {
+    id: number;
+    title: string;
+    description: string;
+    price: number;
+    quantity: number;
+} 
