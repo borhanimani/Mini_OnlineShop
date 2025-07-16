05840d6ef2c0446ca2f82a2497ca6a32c38e666f
Borhan <iborihan91@gmail.com>
2025-07-16 12:25:08 +0330

    Make products search input

--- mini-online-shop/components/search.tsx
+++ mini-online-shop/components/search.tsx
@@ -1,11 +1,10 @@
 import SearchIcon from '@mui/icons-material/Search';
-import { grey, red } from '@mui/material/colors';
 
 export default function Search() {
   return (
-    <div className=''>
-      <SearchIcon sx={{color: "rgb(58, 58, 58)"}}/>
-      <input id="search-input" className="" type="text" placeholder="Search" />
+    <div className='flex flex-row items-center h-10 w-2/8 relative'>
+      <SearchIcon className='w-1/10 absolute' sx={{ color: "rgba(87, 86, 86, 1)" }} />
+      <input id="search-input" className='h-full w-full pl-6 absolute border-2 border-stone-500 rounded-xl focus:border-blue-600 focus:border-3 focus:outline-none' type="text" placeholder="Search" />
     </div>
   );
 }
