262e150f882bdc9d74e19ab71b0776e9c7d30274
Borhan <iborihan91@gmail.com>
2025-07-16 21:01:11 +0330

    Update project

--- mini-online-shop/components/navbar.tsx
+++ mini-online-shop/components/navbar.tsx
@@ -1,14 +1,13 @@
-
-import AccountCircleOutlinedIcon from '@mui/icons-material/AccountCircleOutlined';
+import UserIcon from "./usericon";
 
 export default function Navbar() {
   return (
-    <header className=" flex flex-row justify-between border-3 h-1/8 items-center color-gray">
-      <AccountCircleOutlinedIcon className='border-1' sx={{ width: 45, height: 45, color: "rgba(87, 86, 86, 1)", fontSize: "small"}} />
-      <h1 className="font-bold text-red-500">Online Shop</h1>
-      <div>
-        cart
-        <div className="" >3</div>
+    <header className=" flex flex-row justify-between h-1/8 items-center color-gray bg-neutral-200">
+      <UserIcon />
+      <h1 className="font-bold text-4xl">Online Shop</h1>
+      <div className='flex flex-row mr-15 text-lg items-center gap-1'>
+        Cart
+        <div className="flex justify-center items-center rounded-full border-1 w-7 h-7 bg-orange-700 text-white" >3</div>
       </div>
     </header>
   );
