57a9f5d7effa9bde789bd2d2c19ace241a44e2e3
Borhan <iborihan91@gmail.com>
2025-07-17 19:03:41 +0330

    Create filter functions for search

--- mini-online-shop/components/fliterbar.tsx
+++ mini-online-shop/components/fliterbar.tsx
@@ -5,8 +5,8 @@ import { Filters } from "@/utils/project-types";
 export default function Filterbar({ list, onCategoryChange, onSearchChange }: Filters) {
   return (
     <section className="flex flex-row items-center mt-1 mx-15 gap-20 h-1/8">
-      <Search />
-      <CategoryFilter list={list} onCategoryChange={onCategoryChange}/>
+      <Search onSearchChange={onSearchChange} />
+      <CategoryFilter list={list} onCategoryChange={onCategoryChange} />
     </section>
   );
 }
