cbe939b7a1fce539fe81681b1bba1c4ab3ab8091
Borhan <iborihan91@gmail.com>
2025-07-18 10:20:44 +0330

    Make cart table in cart list file

--- mini-online-shop/components/cartlist.tsx
+++ mini-online-shop/components/cartlist.tsx
@@ -1,3 +1,16 @@
 export default function CartList() {
-  
+  return(
+    <section>
+      <table>
+        <tr>
+          <th>Number</th>
+          <th>Title</th>
+          <th>Description</th>
+          <th>Quantity</th>
+          <th>Price</th>
+          <th>Total Price</th>
+        </tr>
+      </table>
+    </section>
+  );
 }
