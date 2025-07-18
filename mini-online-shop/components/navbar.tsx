845977a74466c6ffaf74adc2d2a246d6991c74dc
Borhan <iborihan91@gmail.com>
2025-07-18 15:34:05 +0330

    Update cart store and fix bugs

--- mini-online-shop/components/navbar.tsx
+++ mini-online-shop/components/navbar.tsx
@@ -1,14 +1,12 @@
 import UserIcon from "./usericon";
+import CartCount from "./cartcount";
 
 export default function Navbar() {
   return (
     <header className=" flex flex-row justify-between h-1/8 items-center color-gray bg-neutral-200 w-full sticky top-0 z-10">
       <UserIcon />
       <h1 className="font-bold text-4xl">Online Shop</h1>
-      <div className='flex flex-row mr-15 text-lg items-center gap-1'>
-        Cart
-        <div className="flex justify-center items-center rounded-full border-1 w-7 h-7 bg-orange-700 text-white" >3</div>
-      </div>
+      <CartCount />
     </header>
   );
 }
--- mini-online-shop/store/useCartStore.ts
+++ mini-online-shop/store/useCartStore.ts
@@ -3,9 +3,20 @@ import { CartItem, CartStore } from "@/utils/project-types";
 
 export const useCartStore = create<CartStore>((set) => ({
     list: [],
-    addItem: (item: CartItem) => set((state) => ({
-        list: [...state.list, item]
-    })),
+    addItem: (item: CartItem) => set((state) => {
+
+        if (state.list.some(theItem => theItem.id == item.id)) {
+            return {
+                list: state.list.map(theItem =>
+                    theItem.id == item.id ? { ...theItem, quantity: theItem.quantity + 1 } : theItem
+                )
+            };
+        } else {
+            return {
+                list: [...state.list, item]
+            };
+        }
+    }),
 
     deleteItem: (id: number) => set((state) => ({
         list: state.list.filter((item) => { return item.id != id })
