6ecb1a4e1dbcfe07e4646e833f87ffb2c597064d
Borhan <iborihan91@gmail.com>
2025-07-18 16:55:09 +0330

    Update cart components

--- mini-online-shop/components/cartlist.tsx
+++ mini-online-shop/components/cartlist.tsx
@@ -1,16 +1,60 @@
+'use client'
+
+import { useCartStore } from "@/store/useCartStore";
+import { CartItemArray } from "@/utils/project-types";
+import { useEffect } from "react";
+
+
 export default function CartList() {
-  return(
-    <section>
-      <table>
-        <tr>
-          <th>Number</th>
-          <th>Title</th>
-          <th>Description</th>
-          <th>Quantity</th>
-          <th>Price</th>
-          <th>Total Price</th>
-        </tr>
-      </table>
-    </section>
+  const list: CartItemArray = useCartStore((state) => state.list);
+  const deletItem = useCartStore((state) => state.deleteItem);
+  const increaseItem = useCartStore((state) => state.increaseItem);
+  const decreaseItem = useCartStore((state) => state.decreaseItem);
+
+  useEffect(() => {
+    console.log(list);
+
+  }, [list])
+  return (
+    <>
+      {list.length == 0 ? (
+        <p>No item selected yet.</p>
+      ) : (
+        <section>
+          <table>
+            <thead>
+              <tr>
+                <th>Number</th>
+                <th>Title</th>
+                <th>Description</th>
+                <th>Quantity</th>
+                <th>Price</th>
+              </tr>
+            </thead>
+            <tbody>
+              {
+                list.map((item, index) => {
+                  return (
+                    <tr key={item.id}>
+                      <th>{index + 1}</th>
+                      <th>{item.title}</th>
+                      <th>{item.description}</th>
+                      <th>
+                        <button onClick={() => decreaseItem(item.id)}>-</button>
+                        {item.quantity}
+                        <button onClick={() => increaseItem(item.id)}>+</button>
+                      </th>
+                      <th>{item.price}</th>
+                      <th><button onClick={() => deletItem(item.id)}>delete</button></th>
+                    </tr>
+                  );
+                })
+              }
+            </tbody>
+          </table>
+        </section>
+      )
+      }
+    </>
   );
 }
--- mini-online-shop/components/navbar.tsx
+++ mini-online-shop/components/navbar.tsx
@@ -1,12 +1,15 @@
 import UserIcon from "./usericon";
 import CartCount from "./cartcount";
+import Link from "next/link";
 
 export default function Navbar() {
   return (
     <header className=" flex flex-row justify-between h-1/8 items-center color-gray bg-neutral-200 w-full sticky top-0 z-10">
       <UserIcon />
       <h1 className="font-bold text-4xl">Online Shop</h1>
-      <CartCount />
+      <Link href='/cart'>
+        <CartCount />
+      </Link>
     </header>
   );
 }
