560994f5b15fb363258dc5f92d8476be4ec62f9a
Borhan <iborihan91@gmail.com>
2025-07-20 16:51:16 +0330

    Edit user icon UI

--- mini-online-shop/components/usericon.tsx
+++ mini-online-shop/components/usericon.tsx
@@ -26,7 +26,7 @@ export default function UserIcon() {
 
     return (
         <div className='flex items-center gap-2 flex-row ml-4 lg:ml-10 '>
-            <button onClick={openIt} className='flex items-center gap-2 flex-row ml-4 lg:ml-10 border-1'>
+            <button onClick={openIt} className='flex gap-2'>
                 <AccountCircleOutlinedIcon className='text-stone-700' style={{ scale: 1.9 }} display={'block'} />
                 <p className='text-gray-700 font-bold ml-1'>Hi, {data?.name.firstname}!</p>
             </button>
